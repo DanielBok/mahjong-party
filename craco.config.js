7111c93a0e3f8cca0d4163e98e13a491b196760f
Daniel Bok <daniel.bok@outlook.com>
2020-08-19 20:52:39 +0800

    Switch over to typescript

---
+++ craco.config.js
@@ -0,0 +1,69 @@
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+const CracoAntDesignPlugin = require("craco-antd");
+const WebpackBar = require("webpackbar");
+const getCSSModuleLocalIdent = require("react-dev-utils/getCSSModuleLocalIdent");
+
+const path = require("path");
+
+const isDev = process.env.NODE_ENV === "development";
+
+const extraWebpackPlugins = isDev
+  ? [
+      new CircularDependencyPlugin({
+        exclude: /node_modules/,
+        failOnError: true,
+        allowAsyncCycles: false,
+        cwd: process.cwd(),
+      }),
+    ]
+  : []; // prod plugins
+
+module.exports = {
+  webpack: {
+    performance: {
+      hints: true,
+    },
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
+    plugins: [new WebpackBar({ profile: true }), ...extraWebpackPlugins],
+  },
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeTheme: {
+          "@primary-color": "#43b02a",
+          "@primary-color-light": "#46d42a",
+          "@primary-color-dark": "#025c02",
+        },
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@footer-height": "140px",
+              "@header-height": "64px",
+              "@header-margin": "5px",
+              "@min-height":
+                "calc(100vh - @header-height - @footer-height - @header-margin)",
+            },
+          },
+        },
+        cssLoaderOptions: {
+          modules: {
+            localIdentName: isDev ? "[path][name]_[local]" : "[hash:base64]",
+            getLocalIdent: (context, localIdentName, localName, options) =>
+              context.resourcePath.includes("node_modules")
+                ? localName
+                : getCSSModuleLocalIdent(
+                    context,
+                    localIdentName,
+                    localName,
+                    options
+                  ),
+          },
+          localsConvention: "camelCase",
+        },
+      },
+    },
+  ],
+};
---
+++ types/app.d.ts
@@ -0,0 +1,4 @@
+declare module "*.svg";
+declare module "*.less";
+
+type LoadingState = "REQUEST" | "SUCCESS" | "FAILURE";
